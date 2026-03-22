d68000f5013d93378f1c5f6ef861bd37d187e3a1
Ashish Wagh <ashok.wagh2005@gmail.com>
2026-03-22 20:06:45 +0530

    Create seven_brothers.py

---
+++ part1/seven_brothers.py
@@ -0,0 +1,10 @@
+# Problem: Seven Brothers
+# Task: Print the names of the seven brothers in alphabetical order
+
+print("Aapo")
+print("Eero")
+print("Juhani")
+print("Lauri")
+print("Simeoni")
+print("Timo")
+print("Tuomas")
