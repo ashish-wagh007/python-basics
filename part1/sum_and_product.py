4953578669200420cfd3da26d66e1a10af88d8ee
Ashish Wagh <ashok.wagh2005@gmail.com>
2026-03-25 19:12:21 +0530

    Create sum_and_product.py

---
+++ part1/sum_and_product.py
@@ -0,0 +1,6 @@
+number1 = int(input("Number 1: "))
+number2 = int(input("Number 2: "))
+sum = number1 + number2
+prod = number1 * number2
+print(f"The sum of the numbers: {sum}")
+print(f"The product of the numbers: {prod}")
