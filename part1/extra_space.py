da179b7001ab1f9fdb2bf3140c15a0af19433598
Ashish Wagh <ashok.wagh2005@gmail.com>
2026-03-25 15:59:35 +0530

    Create extra_space.py

---
+++ part1/extra_space.py
@@ -0,0 +1,19 @@
+name = "Tim Tester"
+age = 20
+skill1 = "python"
+level1 = "beginner"
+skill2 = "java"
+level2 = "veteran"
+skill3 = "programming"
+level3 = "semiprofessional"
+lower = 2000
+upper = 3000
+
+print(f"my name is {name}, I am {age} years old")
+print("")
+print("my skills are")
+print(f" - python ({level1})")
+print(f" - java ({level2})")
+print(f" - programming ({level3})")
+print("")
+print(f"I am looking for a job with a salary of {lower}-{upper} euros per month")
