b6ce3f6d121be6bd2242054601a61c0cdd69599d
Ashish Wagh <ashok.wagh2005@gmail.com>
2026-03-26 13:06:58 +0530

    Create Order_of_magnitude.py

---
+++ part1/Order_of_magnitude.py
@@ -0,0 +1,8 @@
+numb = int(input("Please type in a number: "))
+if numb < 1000:
+    print("This number is smaller than 1000")
+if numb < 100:
+    print("This number is smaller than 100")
+if numb < 10:
+    print("This number is smaller than 10")
+print("Thank you!")
